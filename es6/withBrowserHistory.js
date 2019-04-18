c49a034d023df1cd968683b708b0d1151a39dbc3
Jacky Wijaya <jacky.wijaya@traveloka.com>
2019-04-18 19:57:45 +0700

    Feat: catch up with upstream (#1)
    * fix: fixing add hoist in app.
    
    * 1.1.2
    
    * Update withBrowserHistory for web; Remove redux usage
    
    * Build
    
    * Handle uriPrefix
    
    * Update react to ^16.3.0
    Update deprecated method
    Remove custom uriPrefix and basePath
    
    * Remove log
    
    * Fix spread on render
    
    * Revert react-navigation config
    Replace forwardRef with props navigatorRef
    
    * Add defaultProps for navigatorRef
    
    * Move NavigationService to utils
    
    * fix: handling push, replace, or pop history
    
    * 1.2.0
    
    * fix: hoist router navigation property.
    
    * 1.2.1
    
    * Add navigatorRef handler on withBrowserHistory
    
    * update es6
    
    * 1.2.2
    
    * feat: catch up with upstream

--- es6/withBrowserHistory.js
+++ es6/withBrowserHistory.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+
 export default function stubFunc(W){
-  return ({basePath, ...rest}) => (
-    <W {...rest} />
-  );
+  const Comp = class extends React.Component {
+    render() {
+      const { navigatorRef, basePath, uriPrefix, ...rest } = this.props;
+
+      return (
+        <W
+          ref={ref => {
+            navigatorRef && navigatorRef(ref);
+          }}
+          {...rest}
+        />
+      )
+    }
+  }
+
+  return hoistNonReactStatics(Comp, W);
 };
